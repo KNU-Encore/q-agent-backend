d3a22390e6f93a1cdaa1bd85c5e80de84f3fcee5
skhnpark <skhnpark125@gmail.com>
2025-08-11 15:36:10 +0900

    feat: memory db 생성 및 사용자 입력 데이터 구조 생성

--- main.py
+++ main.py
@@ -1,6 +1,36 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
+
+db = {
+    'analysis_inputs': {},
+    'reports': {},
+}
+
+
+class QueryMetadata(BaseModel):
+    timestamp: str
+    query: str
+    qt: float
+    lt: float
+    rsent: int
+    rexp: int
+
+class ExplainAnalyze(BaseModel):
+    plan_tree: str | dict | None
+
+class DBMetadata(BaseModel):
+    db_info: str | dict | None
+
+class DBSchema(BaseModel):
+    schema_info: str | dict | None
+
+class AnalysisInput(BaseModel):
+    query_metadata: QueryMetadata
+    explain_analyze: ExplainAnalyze
+    db_metadata: DBMetadata
+    db_schema: DBSchema
 
 
 @app.get('/')
